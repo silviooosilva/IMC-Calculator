ac4667b40806e28866678945d32b784c4d27d16f
silviooosilva <66147538+silviooosilva@users.noreply.github.com>
2021-12-19 23:25:35 +0100

    Update main.js

--- IMC-Calculator/assets/js/main.js
+++ IMC-Calculator/assets/js/main.js
@@ -12,11 +12,11 @@ function calcula_IMC(altura, peso){
     FORMULA USADA PARA CALCULAR*/
     show_result.value = IMC.toFixed(2) //FORMATANDO O RESULTADO (APENAS DUAS CASAS DECIMAIS APÓS A VIRGULA(PONTO))
     if(IMC <= 18.5){
-       peso_info.innerHTML = 'Não está no peso ideal'
+       peso_info.innerHTML = 'NÃO ESTÁ NO PESO IDEAL'
     }else if((IMC >= 18.6) && (IMC <= 24.9)){
-        peso_info.innerHTML = 'Peso Ideal'
+        peso_info.innerHTML = 'PESO IDEAL'
     }else if((IMC >= 25) && (IMC <= 29.9)){
-        peso_info.innerHTML = 'lEVEMENTE ACIMA DO PESO'
+        peso_info.innerHTML = 'LEVEMENTE ACIMA DO PESO'
     }else if((IMC >= 30) && (IMC <= 34.9)){
         peso_info.innerHTML = 'OBESIDADE GRAU I'
     }else if((IMC >= 35) && (IMC <= 39.9)){
